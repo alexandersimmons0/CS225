4628961b41d8e03975f42d481e589e9286536a4b
Sachiterasu <teirishwolf@gmail.com>
2023-02-28 21:01:38 -0700

    Moved and edited some scripts

---
+++ Assets/Scripts/gunBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunBehaviour : MonoBehaviour
+{
+    public GameObject bullet;
+    private gameManager gameManager;
+    private Renderer rend;
+    public float BulletSpeed = 100f;
+    private float bulletCount = 12;
+    private float fireRate = 3.0f;
+    private float nextFire = 0.0f;
+    void Start(){
+        gameManager = GameObject.Find("gameManager").GetComponent<gameManager>();
+        rend = GetComponent<MeshRenderer>(); //or whatever render it uses
+        rend.enabled = false;
+    }
+    void FixedUpdate(){
+        if(gameManager.Gun1){
+            rend.enabled = true;
+            if(Input.GetMouseButtonDown(0)&&Time.time>nextFire){
+                nextFire = Time.time + fireRate;
+                GameObject newBullet = Instantiate(bullet, this.transform.position, this.transform.rotation);
+                Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+                BulletRB.velocity = this.transform.forward * BulletSpeed;
+                //Create bullet
+                bulletCount--;
+            }
+            if(Input.GetKeyDown(KeyCode.R)){
+                bulletCount = 12;
+            }
+        }else{
+            rend.enabled = false;
+        }
+    }
+}
