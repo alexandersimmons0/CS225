eeade922a0b00e1b23ce1e24eb211d5e353bbe64
alexandersimmons0 <122643996+alexandersimmons0@users.noreply.github.com>
2023-03-01 14:23:10 -0700

    Update gunBehaviour2.cs

--- Assets/Scripts/gunBehaviour2.cs
+++ Assets/Scripts/gunBehaviour2.cs
@@ -9,22 +9,21 @@ public class gunBehaviour2 : MonoBehaviour
     private Renderer rend;
     public float BulletSpeed = 100f;
     private float bulletCount = 30;
-    private float fireRate = 0.5f;
+    private float fireRate = 0.1f;
     private float nextFire = 0.0f;
     void Start(){
         gameManager = GameObject.Find("gameManager").GetComponent<gameManager>();
         rend = GetComponent<MeshRenderer>(); //or whatever render it uses
         rend.enabled = false;
     }
-    void FixedUpdate(){
+    void Update(){
         if(gameManager.Gun2){
             rend.enabled = true;
-            if(Input.GetMouseButton(0)&&Time.time>nextFire){
+            if(Input.GetMouseButton(0)&&Time.time>nextFire&&bulletCount>=0){
                 nextFire = Time.time + fireRate;
                 GameObject newBullet = Instantiate(bullet, this.transform.position, this.transform.rotation);
                 Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
                 BulletRB.velocity = this.transform.forward * BulletSpeed;
-                //Create Bullet
                 bulletCount--;
             }
             if(Input.GetKeyDown(KeyCode.R)){
